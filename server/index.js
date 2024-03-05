d9ba4639539c3c1ad1af7579d66c21a59f9ac71c
Shivam Prajapati <100841132+itsgeekhokage@users.noreply.github.com>
2024-03-05 21:50:50 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,13 +16,13 @@ const DATABASE_URL = process.env.DATABASE_URL;
 
 
 connectdb(DATABASE_URL);
-app.use(cors());
-// app.use(cors({
-//             origin : ["https://main.d2j3su6c5okp5n.amplifyapp.com"],
-//             methods : ["GET", "POST", "DELETE", "PUT"],
-//             credentials : true
-//         }        
-//     ));
+// app.use(cors());
+app.use(cors({
+            origin : ["https://dummy-crm-4k41.vercel.app/"],
+            methods : ["GET", "POST", "DELETE", "PUT"],
+            credentials : true
+        }        
+    ));
 app.use(express.json({ limit: "30mb" }));
 
 app.use('/auth', auth);
